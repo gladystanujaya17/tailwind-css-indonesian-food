9a61997a44abb32b536851857632abf0baa8e96a
gladystanujaya17 <gladystana17@gmail.com>
2024-04-20 11:09:48 +0700

    first commit

---
+++ public/index.js
@@ -0,0 +1,11 @@
+const burger = document.getElementById('burger');
+const menu = document.getElementById('menu');
+
+burger.addEventListener('click', function() {
+    if (menu.classList.contains('hidden')) {
+        menu.classList.remove('hidden');
+    } else {
+        menu.classList.add('hidden');
+    }
+});
+
